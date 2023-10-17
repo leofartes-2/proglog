bff83920bc131fa3486199c42bcb86353a593014
leofartes-2 <albi.kulla@comcast.net>
2023-10-17 12:25:52 -0400

    Add code to create and start the server

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"log"
+
+	"github.com/leofartes-2/proglog/internal/server"
+)
 
 func main() {
-	fmt.Println("Start your server here...")
+	srv := server.CreateServer(":8080")
+	log.Fatal(srv.ListenAndServe())
+
+	// POST:
+	// curl -X POST localhost:8080/log/write -d '{"record": {"value": "TGV0J3MgR28gIzEK"}}'
+	// GET:
+	// curl -X GET localhost:8080/log/read -d '{"offset": 0}'
 }
